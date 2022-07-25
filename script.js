49334101f048a2fa0dc353bea09ce1825dcfb624
Olukayode <olukayodeazeez2000@gmail.com>
2022-07-25 20:37:56 +0100

    Working on ikonik

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+// NAVBAR
+const openBtn = document.querySelector('.open-btn')
+const closeBtn = document.querySelector('.close-btn')
+const navs = document.querySelectorAll('.nav')
+
+openBtn.addEventListener('click', ()=>{
+    navs.forEach( (nav)=>{
+        nav.classList.add('visible')
+    })
+})
+closeBtn.addEventListener('click', ()=>{
+    navs.forEach( (nav)=>{
+        nav.classList.remove('visible')
+    })
+})
+// FREQUENTLY ASKED QUESTIONS
 const faqsbtn = document.querySelectorAll('.btn')
 
 faqsbtn.forEach((btn)=>{
