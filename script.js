f9c74c23e1fb4b6424126d3347c1192ee457e5bf
Olukayode <olukayodeazeez2000@gmail.com>
2022-07-25 10:05:48 +0100

    Working on Ikonik

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ faqsbtn.forEach((btn)=>{
         btn.parentNode.classList.toggle('active')
     })
 })
+
+const images = document.querySelector('.image-container')
+
+const img = document.querySelectorAll('.image-container .six-img')
+
+let currentImage = 0
+
+let interval = setInterval(run, 2000)
+
+function run(){
+    currentImage++
+
+    changeImage()
+}
+
+function changeImage(){
+    if(currentImage > img.length - 1){
+        currentImage = 0
+    }else if(currentImage < 0){
+        currentImage = img.length - 1
+    }
+
+    images.style.transform = `translateX(${-currentImage * 100}px)`
+}
